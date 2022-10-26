926cdc26c6f51a3b9c684fb6b17c0a33ba4de09a
Kemeker <kemeker@gmail.com>
2022-10-26 01:18:44 -0300

    teste

--- App/interface.py
+++ App/interface.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import tkinter as tk
 import datetime as dt
 import os
-from App.funçao_banco import carrega_lista
+import tkinter.messagebox as msg
 from funçao_banco import *
 
 lista_voltagem = ["110v", "220v", "360v", "110v~220v"]
@@ -13,12 +13,13 @@ lista_tecnicos = carrega_lista('Tecnicos')
 lista_turnos = ["Manha", "Tarde", "Noite"]
 
 '''-----------------------------------------------INTERFACE CADASTRO DO TECNICO---------------------------------------'''
-def janela_cadastro_tecnicos():
-    '''Funçao para cadastrar tecnico'''
-    janela_cadastro = tk.Toplevel()
-    janela_cadastro.geometry("800x400") 
-    janela_cadastro.title("Cadastrar Técnico") 
 
+
+def janela_cadastro_tecnicos():
+    janela_cadastro = tk.Toplevel()
+    janela_cadastro.geometry("800x400")
+    janela_cadastro.title("Cadastrar Técnico")
+    '''------------------------------------------------ADICIONAR INF NO BD------------------------------------------------'''
     def novo_tecnico():
         BD: openpyxl.Workbook = abrir_BD()
         sh = BD.active
@@ -46,72 +47,76 @@ def janela_cadastro_tecnicos():
         janela_cadastro.destroy()
         msg.showinfo('Cadastro de Tecnicos', 'Tecnico Cadastrado!')
 
-    '''-------------------------------------------------Cadastro do Tecnico------------------------------------------------'''
-    label_nome =tk.Label(janela_cadastro, text='Nome do Técnico:')
-    label_nome.grid(row=0, column=0, padx=0, pady=0, columnspan=1) 
+    '''-------------------------------------------------Cadastro do Tecnico'''''''''''''''''''''''''''''''''''''''''''''''
+    label_nome = tk.Label(janela_cadastro, text='Nome do Técnico:')
+    label_nome.grid(row=0, column=0, padx=0, pady=0, columnspan=1)
     entrar_nome = tk.Entry(janela_cadastro)
-    entrar_nome.grid(row=0, column=1, padx=0, pady=0, columnspan=1) 
-    
+    entrar_nome.grid(row=0, column=1, padx=0, pady=0, columnspan=1)
+
     '''---------------------------------------------------cadastro do cpf-------------------------------------------------'''
-    label_cpf =tk.Label(janela_cadastro, text='CPF:')
-    label_cpf.grid(row=2, column=0, padx=0, pady=0, columnspan=1) 
+    label_cpf = tk.Label(janela_cadastro, text='CPF:')
+    label_cpf.grid(row=2, column=0, padx=0, pady=0, columnspan=1)
     entrar_cpf = tk.Entry(janela_cadastro)
-    entrar_cpf.grid(row=2, column=1, padx=0, pady=0, columnspan=1) 
+    entrar_cpf.grid(row=2, column=1, padx=0, pady=0, columnspan=1)
 
-    label_radio =tk.Label(janela_cadastro, text='Nª RADIO:')
-    label_radio.grid(row=3, column=0, padx=0, pady=0, columnspan=1) 
+    label_radio = tk.Label(janela_cadastro, text='Nª RADIO:')
+    label_radio.grid(row=3, column=0, padx=0, pady=0, columnspan=1)
     entrar_radio = tk.Entry(janela_cadastro)
-    entrar_radio.grid(row=3, column=1, padx=0, pady=0, columnspan=1) 
+    entrar_radio.grid(row=3, column=1, padx=0, pady=0, columnspan=1)
 
-    label_turno =tk.Label(janela_cadastro, text='Turno')
-    label_turno.grid(row=4, column=0, padx=0, pady=0, columnspan=1) 
-    entrar_radio = ttk.Combobox(janela_cadastro, values=lista_turnos)
-    entrar_radio.grid(row=4, column=1, padx=0, pady=0, columnspan=1) 
+    label_turno = tk.Label(janela_cadastro, text='Turno')
+    label_turno.grid(row=4, column=0, padx=0, pady=0, columnspan=1)
+    entrar_turno = ttk.Combobox(janela_cadastro, values=lista_turnos)
+    entrar_turno.grid(row=4, column=1, padx=0, pady=0, columnspan=1)
 
-    label_nome_equipe =tk.Label(janela_cadastro, text='Nome da Equipe:')
-    label_nome_equipe.grid(row=5, column=0, padx=0, pady=0, columnspan=1) 
-    entrar_radio = tk.Entry(janela_cadastro)
-    entrar_radio.grid(row=5, column=1, padx=0, pady=0, columnspan=1) 
+    label_nome_equipe = tk.Label(janela_cadastro, text='Nome da Equipe:')
+    label_nome_equipe.grid(row=5, column=0, padx=0, pady=0, columnspan=1)
+    entrar_equipe = tk.Entry(janela_cadastro)
+    entrar_equipe.grid(row=5, column=1, padx=0, pady=0, columnspan=1)
 
-    
-    botao_cadastrar_tecnico = tk.Button(janela_cadastro, text='Cadastrar')
-    botao_cadastrar_tecnico.grid(row=6, column=3, padx=0, pady=0, columnspan=1) 
+    botao_cadastrar_tecnico = tk.Button(janela_cadastro, text='Cadastrar', command=novo_tecnico)
+    botao_cadastrar_tecnico.grid(row=6, column=3, padx=0, pady=0, columnspan=1)
+
+
 '''-----------------------------------------------INTERFACE PARA CADASTRO DE FERRAMENTAS-----------------------------'''
+
+
 def janela_cadastro_ferramentas():
     janela_cadastro = tk.Toplevel()
-    janela_cadastro.geometry("800x400") 
+    janela_cadastro.geometry("800x400")
     janela_cadastro.title("Cadastrar Ferramenta")
-      
-
-    
 
     label_descricao = tk.Label(janela_cadastro, text="Descrição da ferramenta")
-    label_descricao.grid(row=1, column=0,padx = 10, pady=10, sticky='nswe', columnspan =4 )
+    label_descricao.grid(row=1, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
 
     entry_descricao = tk.Entry(janela_cadastro)
-    entry_descricao.grid(row=2,column=0, padx=10, pady=10, sticky='nswe', columnspan = 4)
+    entry_descricao.grid(row=2, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
 
     label_tipo_unidade = tk.Label(janela_cadastro, text="Código da ferramenta")
-    label_tipo_unidade.grid(row=3, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+    label_tipo_unidade.grid(row=3, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
     combobox_selecionar_tipo = ttk.Combobox(janela_cadastro, values=lista_tipos)
-    combobox_selecionar_tipo.grid(row=3, column=2, padx = 10, pady=10, sticky='nswe', columnspan = 2)
+    combobox_selecionar_tipo.grid(row=3, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    label_quant =  tk.Label(janela_cadastro, text="Quantia em estoque")
-    label_quant.grid(row=4, column=0,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+    label_quant = tk.Label(janela_cadastro, text="Quantia em estoque")
+    label_quant.grid(row=4, column=0, padx=10, pady=10, sticky='nswe', columnspan=2)
 
-    entry_quant =  tk.Entry(janela_cadastro)
-    entry_quant.grid(row=4, column=2,padx = 10, pady=10, sticky='nswe', columnspan =2 )
+    entry_quant = tk.Entry(janela_cadastro)
+    entry_quant.grid(row=4, column=2, padx=10, pady=10, sticky='nswe', columnspan=2)
 
     botao_criar_codigo = tk.Button(janela_cadastro, text="Cadastrar", command=inserir_codigo)
-    botao_criar_codigo.grid(row=5,column=0,padx = 10, pady=10,sticky='nswe', columnspan =4)
+    botao_criar_codigo.grid(row=5, column=0, padx=10, pady=10, sticky='nswe', columnspan=4)
+
+
 '''-----------------------------------------------INTERFACE PARA FAZER SOLICITAÇAO DAS FERRAMENTAS--------------------------------'''
+
+
 def janela_solicitacoes():
     '''funçao para solicitar'''
     janela = tk.Toplevel()
-    janela.geometry("800x400") 
-    janela.title("Solicitaçao de Ferramentas") 
-    
+    janela.geometry("800x400")
+    janela.title("Solicitaçao de Ferramentas")
+
     def nova_solicitacao():
         BD: openpyxl.Workbook = abrir_BD()
         sh = BD.active
@@ -119,7 +124,7 @@ def janela_solicitacoes():
         colF = 0
         novaLinha = 0
         for col in range(1, 100, 1):
-            if sh.cell(row=1, column=col).value == 'Tecnico':
+            if sh.cell(row=1, column=col).value == 'Nome':
                 colI = col
                 break
         for col in range(colI, 100, 1):
@@ -137,7 +142,7 @@ def janela_solicitacoes():
             contador +=1
         fechar_BD(BD)
         janela.destroy()
-        msg.showinfo('Cadastrar Solicitaçao', 'Solicitaçao Cadastrada!')
+        msg.showinfo('Solicitar', 'Solicitacao Concluida!')
 
     label_nome_tecnico = tk.Label(janela, text="Técnico:")
     label_nome_tecnico.grid(row=0, column=0, padx=10, pady=10)
@@ -172,17 +177,17 @@ def janela_solicitacoes():
     entry_data_entrada = tk.Entry(janela)
     entry_data_entrada.grid(row=2, column=3, padx=10, pady=10, columnspan=1)
     '''------------------------------------------------------------------------------------------------------------------'''
-    
     '''--------------------------------------Botao para cadastrar a solicitaçao--------------------------------------'''
-    botao_cadastrar_tecnico = tk.Button(janela, text='Cadastrar')
-    botao_cadastrar_tecnico.grid(row=4, column=2, padx=0, pady=0, columnspan=1) 
+    botao_cadastrar_solicitacao = ttk.Button(janela, text="Cadastrar Solicitaçao")
+    botao_cadastrar_solicitacao.grid(row=4, column=1, sticky=tk.E, padx=5, pady=5)
 
     '''-----------------------------------------------INTERFACE PRINCIPAL DA APLICAÇAO-------------------------------------'''
 
+
 '''-----------------------------------------------INTERFACE PRINCIPAL DO SISTEMA-----------------------------------------------'''
-aplication = tk.Tk() 
-aplication.geometry("900x600") 
-aplication.title("DBV Softwares & Sistemas") 
+aplication = tk.Tk()
+aplication.geometry("900x600")
+aplication.title("DBV Softwares & Sistemas")
 
 nome_cadastro = tk.Label(aplication, text='CADASTRAR FERRAMENTAS')
 nome_cadastro.grid(row=0, column=6, padx=0, pady=0, columnspan=1)
@@ -193,27 +198,24 @@ nome_tecnico.grid(row=0, column=4, padx=0, pady=0, columnspan=1)
 nome_solicitacao = tk.Label(aplication, text='SOLICITAR FERRAMENTA')
 nome_solicitacao.grid(row=0, column=1, padx=0, pady=0, columnspan=1)
 
-
 '''-------------------------------------------------Botoes para direcionar a paginas------------------------------------'''
 imagem_solicitar_ferramenta = tk.PhotoImage(file=r'App\imagens\Solicitar ferramenta.png')
 botao_solicitar_ferramentas = ttk.Button(aplication, image=imagem_solicitar_ferramenta, command=janela_solicitacoes)
 botao_solicitar_ferramentas.place()
 botao_solicitar_ferramentas.grid(row=2, column=1, padx=0, pady=0, columnspan=1)
 
-
 imagem_cadastro_tecnico = tk.PhotoImage(file=r'App\imagens\tecnico.png')
 botao_cadastrar_tecnico = ttk.Button(aplication, image=imagem_cadastro_tecnico, command=janela_cadastro_tecnicos)
 botao_cadastrar_tecnico.place()
 botao_cadastrar_tecnico.grid(row=2, column=4, padx=0, pady=0, columnspan=1)
 
-
 imagem_cadastro_ferramenta = tk.PhotoImage(file=r'App\imagens\cadastrar ferramenta.png')
-botao_cadastrar_ferramenta = ttk.Button(aplication, image=imagem_cadastro_ferramenta, command=janela_cadastro_ferramentas)
+botao_cadastrar_ferramenta = ttk.Button(aplication, image=imagem_cadastro_ferramenta,
+                                        command=janela_cadastro_ferramentas)
 botao_cadastrar_ferramenta.place()
 botao_cadastrar_ferramenta.grid(row=2, column=6, padx=0, pady=0, columnspan=1)
 
 '''--------------------------------------------Photo Image-----------------------------------------'''
-
 
 aplication.mainloop()
 
